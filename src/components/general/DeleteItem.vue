af47254b6296bf3cfb78c173eca6f71c08d8fa78
Rodrigo Alvarenga <ralvarenga@Rodrigos-MacBook-Pro.local>
2021-01-25 22:14:36 -0600

    :sparkles: Added delete modal

---
+++ src/components/general/DeleteItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <modal :show="showModal">
+    <div>
+      <div class="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
+        <div class="sm:flex sm:items-start">
+          <div
+            class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10"
+          >
+            <svg
+              class="w-6 h-6 text-red-600"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+          </div>
+          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3
+              id="modal-headline"
+              class="text-lg font-medium leading-6 text-gray-900"
+            >
+              {{ `${remove ? 'Remover' : 'Eliminar'} ${title}` }}
+            </h3>
+            <div class="mt-2">
+              <p
+                class="text-sm leading-5 text-gray-500"
+                v-html="description"
+              >
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
+        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+          <button
+            type="button"
+            class="inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red sm:text-sm sm:leading-5"
+            @click="actionDelete"
+          >{{ remove ? 'Remover' : 'Eliminar' }}</button>
+        </span>
+        <span class="flex w-full mt-3 rounded-md shadow-sm sm:mt-0 sm:w-auto">
+          <button
+            type="button"
+            class="inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue sm:text-sm sm:leading-5"
+            @click="showModal = false"
+          >Cancelar</button>
+        </span>
+      </div>
+    </div>
+  </modal>
+</template>
+
+<script lang="ts">
+import {
+  Vue, Component, Prop, PropSync,
+} from 'vue-property-decorator';
+import Modal from '@/components/ui/Modal.vue';
+
+@Component({
+  components: {
+    Modal,
+  },
+})
+export default class DeleteItem extends Vue {
+  @Prop({ type: Boolean, default: false }) readonly show!: boolean
+  @Prop(String) readonly title!: string
+  @Prop({ type: String, default: '¿Estás seguro de que deseas eliminar este registro? <br> Todos sus datos serán eliminados permanentemente. Esta acción no se puede deshacer.' }) readonly description!: string;
+  @Prop({ type: Boolean, default: false }) readonly remove!: boolean;
+  @PropSync('show', Boolean) showModal!: boolean
+  actionDelete() {
+    this.showModal = false;
+    this.$emit('action');
+  }
+}
+</script>
